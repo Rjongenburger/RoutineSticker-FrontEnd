2655b444a6664001dacfd8c232bedf1a94aa8212
Richard <richardjongenburger@hotmail.com>
2017-05-01 18:15:09 +0200

    Initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from "app/pages/home/home.component";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
